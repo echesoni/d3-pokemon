92f5a440067a71ecf207ca89992fe45d52a284ba
eches <Elaine.Chesoni83@gmail.com>
2019-06-13 11:25:16 -0700

    added api routes, on-click filters

--- public/js/index.js
+++ public/js/index.js
@@ -1,25 +1,237 @@
-console.log("Sanity check: JS is working!");
+var pokemonData = [];
+var data = {};
 
-var $pokemonList;
-var allPokemon = [];
-
-$(document).ready(function(){
-  $pokemonList = $('#pokemonList');
-
-  $.ajax({
-    method: 'GET',
-    url: 'http://localhost:8080/pokemon.json#',
-    success: handleSuccess,
-    error: handleError
-  });
-});
+var diameter = 600;
+var color = d3.scaleOrdinal(d3.schemeYlGnBu[7]);
 
 function handleSuccess(json) {
   allPokemon = json;
-  render();
-}
+  allPokemon.forEach(function(pokemon){
+    pokemonData.push(pokemon);
+  });
+  data = {"children":pokemonData}
+  console.log(data);
+  setChart(data);
+};
 
 function handleError(err) {
   console.log("There was an issue trying to catch 'em all");
   $('#pokemonList').text("Failed to load pokemon, is the server working?");
 }
+
+var setChart = function(dataset){
+
+  var bubble = d3.pack(dataset)
+      .size([diameter, diameter])
+      .padding(1.5);
+
+  var svg = d3.select("#bubble-box")
+      .append("svg")
+      .attr("width", diameter)
+      .attr("height", diameter)
+      .attr("class", "bubble")
+      .attr("id", "currentChart");
+
+  var nodes = d3.hierarchy(dataset)
+      .sum(function(d) { return d.SpecialAttack; });
+
+  var node = svg.selectAll(".node")
+      .data(bubble(nodes).descendants())
+      .enter()
+      .filter(function(d){
+          return  !d.children
+      })
+      .append("g")
+      .attr("class", "node")
+      .attr("transform", function(d) {
+          return "translate(" + d.x + "," + d.y + ")";
+      });
+
+  node.append("title")
+      .text(function(d) {
+          return d.Name + ": " + d.SpecialAttack;
+      });
+
+  node.append("circle")
+      .attr("r", function(d) {
+          return d.r;
+      })
+      .style("fill", function(d,i) {
+          return color(i);
+      });
+
+  node.append("text")
+      .attr("dy", ".2em")
+      .style("text-anchor", "middle")
+      .text(function(d) {
+          return d.data.Name.substring(0, d.r / 3);
+      })
+      .attr("font-family", "sans-serif")
+      .attr("font-size", function(d){
+          return d.r/5;
+      })
+      .attr("fill", "white");
+
+  node.append("text")
+      .attr("dy", "1.3em")
+      .style("text-anchor", "middle")
+      .text(function(d) {
+          return d.data.SpecialAttack;
+      })
+      .attr("font-family",  "Gill Sans", "Gill Sans MT")
+      .attr("font-size", function(d){
+          return d.r/5;
+      })
+      .attr("fill", "white");
+
+  d3.select(self.frameElement)
+      .style("height", diameter + "px");
+};
+
+var removeCurrentStats = function(){
+  $(".node").remove();
+  $("#currentChart").remove();
+};
+
+$(document).ready(function(){
+
+  $pokemonList = $('#pokemonList');
+
+  $.ajax({
+    method: 'GET',
+    url: '/api/dataset',
+    success: handleSuccess,
+    error: handleError
+  });
+
+});
+
+$("#filter1").click(function(e){
+  e.preventDefault();
+  // remove nodes
+  removeCurrentStats();
+
+  $.ajax({
+    method: 'GET',
+    url: '/api/waterPokemon',
+    success: handleSuccess,
+    error: handleError
+  });
+
+  // reset chart color
+  color = d3.scaleOrdinal(d3.schemeYlGnBu[7]);
+
+});
+
+$("#filter2").click(function(e){
+  e.preventDefault();
+
+  // remove existing nodes
+  removeCurrentStats();
+
+  $.ajax({
+    method: 'GET',
+    url: '/api/firePokemon',
+    success: handleSuccess,
+    error: handleError
+  });
+  // reset chart color
+  color = d3.scaleOrdinal(d3.schemeReds[7]);
+});
+
+$("#filter3").click(function(e){
+  e.preventDefault();
+
+  // remove existing nodes
+  removeCurrentStats();
+
+
+  $.ajax({
+    method: 'GET',
+    url: '/api/grassPokemon',
+    success: handleSuccess,
+    error: handleError
+  });
+  // reset chart color
+  color = d3.scaleOrdinal(d3.schemeBuGn[7]);
+});
+
+$("#filter4").click(function(e){
+  e.preventDefault();
+
+  // remove existing nodes
+  removeCurrentStats();
+
+  $.ajax({
+    method: 'GET',
+    url: '/api/poisonPokemon',
+    success: handleSuccess,
+    error: handleError
+  });
+  // reset chart color
+  color = d3.scaleOrdinal(d3.schemeBuPu[7]);
+});
+
+$("#filter5").click(function(e){
+  e.preventDefault();
+
+  // remove existing nodes
+  removeCurrentStats();
+
+  $.ajax({
+    method: 'GET',
+    url: '/api/psychicPokemon',
+    success: handleSuccess,
+    error: handleError
+  });
+  // reset chart color
+  color = d3.scaleOrdinal(d3.schemeRdPu[7]);
+});
+
+$("#filter6").click(function(e){
+  e.preventDefault();
+
+  // remove existing nodes
+  removeCurrentStats();
+
+  $.ajax({
+    method: 'GET',
+    url: '/api/icePokemon',
+    success: handleSuccess,
+    error: handleError
+  });
+  // reset chart color
+  color = d3.scaleOrdinal(d3.schemeBlues[7]);
+});
+
+$("#filter7").click(function(e){
+  e.preventDefault();
+
+  // remove existing nodes
+  removeCurrentStats();
+
+  $.ajax({
+    method: 'GET',
+    url: '/api/electricPokemon',
+    success: handleSuccess,
+    error: handleError
+  });
+  // reset chart color
+  color = d3.scaleOrdinal(d3.schemeYlOrBr[7]);
+});
+
+$("#filter8").click(function(e){
+  e.preventDefault();
+
+  // remove existing nodes
+  removeCurrentStats();
+
+  $.ajax({
+    method: 'GET',
+    url: '/api/flyingPokemon',
+    success: handleSuccess,
+    error: handleError
+  });
+  // reset chart color
+  color = d3.scaleOrdinal(d3.schemeBrBG[7]);
+});
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fetch = require("node-fetch");
+const fs = require('fs');
 const port = 3000;
 
 // create express app
@@ -8,9 +9,6 @@ const app = express();
 
 // serve static files in public
 app.use(express.static('public'));
-
-// Allow Cross Origin Access
-fetch('http://localhost:8080/pokemon.json#', { mode: 'no-cors' });
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -22,28 +20,108 @@ app.use(bodyParser.json());
 const dbConfig = require('./config/database.config.js');
 const mongoose = require('mongoose');
 
-mongoose.Promise = global.Promise;
+// mongoose.Promise = global.Promise;
 
 // Connect to the database
-mongoose.connect(dbConfig.url, function(err, db) {
-  useNewUrlParser: true,
-  console.log("Here it is: " + db.collection("pokemons").findOne({}));
-}).then(() => {
-  console.log("Successfully connected to the database");
-}).catch(err => {
-  console.log('Could not connect to the database. Exiting now...', err);
-  process.exit();
-});
+// mongoose.connect(dbConfig.url, function(err, db) {
+//   useNewUrlParser: true
+//   // console.log("Here it is: " + db.collection("pokemons").findOne({}));
+// }).then(() => {
+//   console.log("Successfully connected to the database");
+// }).catch(err => {
+//   console.log('Could not connect to the database. Exiting now...', err);
+//   process.exit();
+// });
 
 // define route
 app.get('/', (req, res) => {
-  // Allow Cross Origin Access
-  res.setHeader('Access-Control-Allow-Origin', '*');
   res.sendFile('views/index.html' , { root : __dirname});
 });
 
+// app.get('/test', (req, res) => {
+//   res.sendFile('views/test.html' , { root : __dirname});
+// });
+
+// Api workaround for build/testing
+app.get('/api/pokemon', (req, res) => {
+  fs.readFile('data/pokemon.json', (err, json) => {
+    let obj = JSON.parse(json);
+    res.json(obj);
+  });
+});
+
+app.get('/api/dataset', (req, res) => {
+  fs.readFile('data/dataset.json', (err, json) => {
+    let obj = JSON.parse(json);
+    res.json(obj);
+  });
+});
+
+app.get('/api/dataset3', (req, res) => {
+  fs.readFile('data/dataset3.json', (err, json) => {
+    let obj = JSON.parse(json);
+    res.json(obj);
+  });
+});
+
+app.get('/api/firePokemon', (req, res) => {
+  fs.readFile('data/firePokemon.json', (err, json) => {
+    let obj = JSON.parse(json);
+    res.json(obj);
+  });
+});
+
+app.get('/api/waterPokemon', (req, res) => {
+  fs.readFile('data/waterPokemon.json', (err, json) => {
+    let obj = JSON.parse(json);
+    res.json(obj);
+  });
+});
+
+app.get('/api/grassPokemon', (req, res) => {
+  fs.readFile('data/grassPokemon.json', (err, json) => {
+    let obj = JSON.parse(json);
+    res.json(obj);
+  });
+});
+
+app.get('/api/poisonPokemon', (req, res) => {
+  fs.readFile('data/PoisonPokemon.json', (err, json) => {
+    let obj = JSON.parse(json);
+    res.json(obj);
+  });
+});
+
+app.get('/api/psychicPokemon', (req, res) => {
+  fs.readFile('data/psychicPokemon.json', (err, json) => {
+    let obj = JSON.parse(json);
+    res.json(obj);
+  });
+});
+
+app.get('/api/icePokemon', (req, res) => {
+  fs.readFile('data/icePokemon.json', (err, json) => {
+    let obj = JSON.parse(json);
+    res.json(obj);
+  });
+});
+
+app.get('/api/electricPokemon', (req, res) => {
+  fs.readFile('data/electricPokemon.json', (err, json) => {
+    let obj = JSON.parse(json);
+    res.json(obj);
+  });
+});
+
+app.get('/api/flyingPokemon', (req, res) => {
+  fs.readFile('data/flyingPokemon.json', (err, json) => {
+    let obj = JSON.parse(json);
+    res.json(obj);
+  });
+});
+
 // api routes
-require('./app/routes/pokemon.routes.js')(app);
+// require('./app/routes/pokemon.routes.js')(app);
 
 // listen for requests
 app.listen(port, () => {
